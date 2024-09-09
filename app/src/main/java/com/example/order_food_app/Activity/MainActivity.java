9dc86c9187a246da4f8d2355e4796c5b0d68d792
HoangNguyetHa0310 <hoangyh2712@gmail.com>
2024-09-09 09:45:26 +0700

    1.14

--- app/src/main/java/com/example/order_food_app/Activity/MainActivity.java
+++ app/src/main/java/com/example/order_food_app/Activity/MainActivity.java
@@ -10,9 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 
 import com.example.order_food_app.Adapter.CategoryAdapter;
+import com.example.order_food_app.Adapter.SliderAdapter;
 import com.example.order_food_app.Domain.Category;
+import com.example.order_food_app.Domain.SliderItems;
 import com.example.order_food_app.R;
 import com.example.order_food_app.databinding.ActivityMainBinding;
 import com.google.firebase.database.DataSnapshot;
@@ -34,6 +39,52 @@ public class MainActivity extends BaseActivity {
 
 
         initCategory();
+        initBanner();
+        setVariable();
+    }
+
+    private void initBanner() {
+        DatabaseReference myRef = database.getReference("Banners");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<SliderItems> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue:snapshot.getChildren()) {
+                        items.add(issue.getValue(SliderItems.class));
+                    }
+                    banners(items);
+                    binding.progressBarBanner.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void banners(ArrayList<SliderItems> items) {
+        binding.viewPager2.setAdapter(new SliderAdapter(items,binding.viewPager2));
+        binding.viewPager2.setClipChildren(false);
+        binding.viewPager2.setClipToPadding(false);
+        binding.viewPager2.setOffscreenPageLimit(3);
+        binding.viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+
+        binding.viewPager2.setPageTransformer(compositePageTransformer);
+
+    }
+
+    private void setVariable() {
+        binding.bottomMenu.setItemSelected(R.id.home, true);
+
     }
 
     private void initCategory() {
